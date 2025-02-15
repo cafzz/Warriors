3dc6990ad2ed62c70fee0e2d85c39f29aa231178
cafzz <169160466+cafzz@users.noreply.github.com>
2025-02-15 16:09:56 -0300

    f

---
+++ script.js
@@ -0,0 +1,9 @@
+function MudarCor(){
+    const cor = document.getElementById(cor).value;
+    const imagemCamisa = document.querySelector(".Camisa img");
+    const imagens = {
+"Off White": "WRS-BSC-OVR.webp",
+"marrom": "WRS-BSC-OVR2.webp",
+"Preta": ""
+    }
+}
